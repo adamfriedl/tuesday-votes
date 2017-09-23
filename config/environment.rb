87ed37266e7a58e631bb37c002619be34584abea
Adam Friedl <ajfriedl@gmail.com>
2017-09-23 15:35:26 -0400

    Production db not working on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,39 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# ActiveRecord::Base.establish_connection(
-#   adapter: "postgresql",
-#   database: "db/#{ENV['SINATRA_ENV']}"
-# )
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "db/#{ENV['SINATRA_ENV']}"
+)
 
-# require_all 'app'
-
-configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-
-  # set :database, ENV['SINATRA_ENV']
-  # set :show_exceptions, true
-
-  ActiveRecord::Base.establish_connection(
-    adapter: "postgresql",
-    database: "db/#{ENV['SINATRA_ENV']}"
-  )
-
- end
-
- ENV['SINATRA_ENV'] ||= "production"
- 
- configure :production do
-  # ENV['SINATRA_ENV'] ||= "production"
-
-  set :database, ENV['SINATRA_ENV']
-  set :show_exceptions, true
-
-  ActiveRecord::Base.establish_connection(
-    adapter: "postgresql",
-    database: "db/#{ENV['SINATRA_ENV']}"
-  )
-
- end
-
- require_all 'app'
+require_all 'app'
