4aa46b46036f3b4317017d8fc97d569fe9a7ed6a
Adam Friedl <ajfriedl@gmail.com>
2017-09-24 14:44:38 -0400

    updated v_r method

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -3,6 +3,6 @@ class Volunteer < ActiveRecord::Base
   has_many :political_interests, through: :volunteer_political_interests
 
   def voter_registration
-    Voter.find_by(self.first_name + " " + self.last_name)
+    Voter.find_by_name(self.first_name + " " + self.last_name)
   end
 end
