69a4f106d488024eec5d7408f90ac5dc6e76ca5e
Adam Friedl <ajfriedl@gmail.com>
2017-09-24 15:22:21 -0400

    admin page working. now to build table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 50.times do
   Voter.create([{
-    name: Faker::Name.name,
+    name: Faker::Name.first_name + " " + Faker::Name.last_name,
     age: Faker::Number.number(2),
     location: Faker::Address.city,
     phone_number: Faker::PhoneNumber.phone_number,
