b8d0067bdf8d8a239e7d9b30c77f677539f247ba
Adam Friedl <ajfriedl@gmail.com>
2017-09-24 14:32:26 -0400

    created voter_registration method

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,17 +2,7 @@ class Volunteer < ActiveRecord::Base
   has_many :volunteer_political_interests
   has_many :political_interests, through: :volunteer_political_interests
 
-  def match_voters(voter_array)
-    matches = []
-    voter_array.each do |voter|
-      voter_sep_names = voter.name.split(' ')
-      voter_last_name = voter_sep_names.last
-      if self.last_name.match(voter_last_name)
-        matches << self
-      end
-    end
-    return matches
+  def voter_registration
+    Voter.find_by(self.first_name + " " + self.last_name)
   end
-
-
 end
