bc32cde3cf0714abfcd032eb93d574fdbcc2ec99
Adam Friedl <ajfriedl@gmail.com>
2017-09-24 18:14:48 -0400

    validate presence of names on volunteer model

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,6 +2,8 @@ class Volunteer < ActiveRecord::Base
   has_many :volunteer_political_interests
   has_many :political_interests, through: :volunteer_political_interests
 
+  validates_presence_of :first_name, :last_name
+
   def voter_registration
     Voter.find_by_name(self.first_name + " " + self.last_name)
   end
